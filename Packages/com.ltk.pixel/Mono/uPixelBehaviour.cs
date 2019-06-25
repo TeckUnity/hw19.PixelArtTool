934af2195ac636a7b5c098e8ce2739cac6d97808
John Dickinson <johnd@unity3d.com>
2019-06-25 16:33:37 +0200

    Added MonoBehaviour stub in its own assembly

---
+++ Packages/com.ltk.pixel/Mono/uPixelBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class uPixelBehaviour : MonoBehaviour
+{
+
+    public uPixelCanvas Canvas;
+
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        
+    }
+    
+}
