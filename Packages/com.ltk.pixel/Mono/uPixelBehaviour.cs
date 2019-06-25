fb6004d86518035bdc876f1f1f2b2a7cefc5f588
John Dickinson <johnd@unity3d.com>
2019-06-25 16:52:08 +0200

    MonoBehaviour now has SpriteRenderer and can display sprite created from Canvas Texture2D

--- Packages/com.ltk.pixel/Mono/uPixelBehaviour.cs
+++ Packages/com.ltk.pixel/Mono/uPixelBehaviour.cs
@@ -2,19 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[ExecuteInEditMode]
 public class uPixelBehaviour : MonoBehaviour
 {
 
     public uPixelCanvas Canvas;
 
+    private SpriteRenderer m_SpriteRenderer;
+
     void Start()
     {
-        
+        // Make sure we have a SpriteRenderer
+        m_SpriteRenderer = GetComponent<SpriteRenderer>();
+        if (m_SpriteRenderer == null)
+        {
+            m_SpriteRenderer = gameObject.AddComponent<SpriteRenderer>();
+        }
     }
     
     void Update()
     {
-        
+        // TODO animation
+        if (m_SpriteRenderer.sprite == null && Canvas != null)
+        {
+            var tex = Canvas.ToTexture2D();
+            m_SpriteRenderer.sprite = Sprite.Create(tex, new Rect(0, 0, tex.width, tex.height),
+                new Vector2(0.5f, 0.5f));
+        }
     }
     
 }
